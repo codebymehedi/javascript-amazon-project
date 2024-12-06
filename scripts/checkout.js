4f92426f2122ebd9f85808a1c7c5a2c229db51f4
codebymehedi <mehedihasan2252@gmail.com>
2024-12-06 16:48:50 +0600

    Learn error handling

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,15 +7,22 @@ import { loadCart } from '../data/cart.js';
 
 
 async function loadPage() {
-  
+  try{
+   // throw 'error1';
 
-  await loadProductsFetch();
+    await loadProductsFetch();
 
-  const value = await new Promise((resolve) => {
+  const value = await new Promise((resolve, reject) => {
+   // throw 'error2';
     loadCart(() => {
-     resolve('value3');
+     // reject('error3');
+      resolve('value3');
     });
    });
+  }
+  catch(error) {
+    console.log('Unexpected error. Please try again later.');
+  }
 
    renderOrderSummary();
    renderPaymentSummary();
